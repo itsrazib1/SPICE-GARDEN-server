9f2d290341de536545fc722f3b68f70a6782cf72
itsrazib1 <itsrazib1@gmail.com>
2023-05-03 18:18:49 +0600

    chenged linkes

--- index.js
+++ index.js
@@ -1,57 +1,26 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
-const chefData = require('./chef-server-data.json');
-const chefServer = require('./chef-server.json');
-const slide = require('./chef-slide.json')
+const chefs = require('./data/chef.json')
 
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Chef Recipe data is running');
+  res.send('the-recipe-room');
 });
-app.get('/chefRecipes', (req, res) => {
-  res.send(chefData);
-});
-app.get('/slides', (req, res) => {
-  res.send(slide);
-});
+
 app.get('/chefs', (req, res) => {
-  res.send(chefServer);
+  res.send(chefs);
 });
 
-app.get('/chefRecipes/:id', (req, res) => {
-  const id = req.params.id;
-  const item = chefData.find((pd) => pd.id === id);
-  if (!item) {
-    res.status(404).send('Recipe not found');
-  } else {
-    res.send(item);
-  }
-});
-
-app.get('/slides/:id', (req, res) => {
-    const id = req.params.id;
-    const item = slide.find((pd) => pd.id === id);
-    if (!item) {
-      res.status(404).send('Chef not found');
-    } else {
-      res.send(item);
-    }
-  });
-  
 app.get('/chefs/:id', (req, res) => {
-  const id = req.params.id;
-  const item = chefServer.find((pd) => pd.id === id);
-  if (!item) {
-    res.status(404).send('Chef not found');
-  } else {
-    res.send(item);
-  }
-});
-
+  const id = parseInt(req.params.id);
+  console.log(id)
+  const selectedChef = chefs.find(c => c.chef_id === id);
+  res.send(selectedChef);
+})
 
 app.listen(port, () => {
-  console.log(`App is running on port ${port}`);
-});
+  console.log(`the recipe room is running on ${port}`)
+})
